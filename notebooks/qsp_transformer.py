7f8328e7c4fb62b6edf5f940e98337e8da1bd4c3
Joel Eliason <joel.eliason119@gmail.com>
2025-01-23 19:28:37 -0700

    mixed precision.

--- notebooks/qsp_transformer.py
+++ notebooks/qsp_transformer.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	global_state.set_global_attribute("torch_device", device)
-	keras.mixed_precision.set_global_policy("mixed_bfloat16")
+	# keras.mixed_precision.set_global_policy("mixed_bfloat16")
 
 
 	train, validation, _ = data_loader()
